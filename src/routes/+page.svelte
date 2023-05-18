d72a1fb35e579c7bf95d1840b59290f1620e880e
Muhammad Ali <74698195+alibehram-ai@users.noreply.github.com>
2023-05-18 13:41:47 +0500

    first-comiit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,99 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div class="relative h-full isolate overflow-hidden bg-gradient-to-r from-[#1D267D] via-gray-[#0C134F] to-[#1D267D] text-white py-24 sm:py-32 my-5 mx-5 rounded-lg">
+		
+		<div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
+		  <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+		</div>
+		<div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
+		  <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+		</div>
+		<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		  <div class="mx-auto max-w-2xl lg:mx-0">
+			<h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+			<p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+		  </div>
+		  <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+			<div class="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+			  <a href="#sell">Start a Company <span aria-hidden="true">&rarr;</span></a>
+			  <a href="#contributions">Full Time Support <span aria-hidden="true">&rarr;</span></a>
+			  <a href="#pitches">Pitches <span aria-hidden="true">&rarr;</span></a>
+			  <a href="#contacts">Meet our leadership <span aria-hidden="true">&rarr;</span></a>
+			</div>
+			<dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+			  <div class="flex flex-col-reverse">
+				<dt class="text-base leading-7 text-gray-300">Total Companies</dt>
+				<dd class="text-2xl font-bold leading-9 tracking-tight text-white">12</dd>
+			  </div>
+			  <div class="flex flex-col-reverse">
+				<dt class="text-base leading-7 text-gray-300">Full-time Support</dt>
+				<dd class="text-2xl font-bold leading-9 tracking-tight text-white">300+</dd>
+			  </div>
+			  <div class="flex flex-col-reverse">
+				<dt class="text-base leading-7 text-gray-300">Pitches per week</dt>
+				<dd class="text-2xl font-bold leading-9 tracking-tight text-white">40</dd>
+			  </div>
+			  <div class="flex flex-col-reverse">
+				<dt class="text-base leading-7 text-gray-300">Investing Opportunites</dt>
+				<dd class="text-2xl font-bold leading-9 tracking-tight text-white">Unlimited</dd>
+			  </div>
+			</dl>
+		  </div>
+		</div>
+	  </div>
+	  <div class="bg-[#010836] relative px-5 py-10 lg:px-20 md:px-10 sm:px-5 lg:py-10 ">
+		<div class=" ">
+			<div class="text-[#D4ADFC]  font-bold text-xl" id="sell">Sell</div>
+			<h2 class="mt-5 text-[42px] text-white ">
+				Nodes and relayers for all your favourite protocols
+			</h2>
+			
+		</div>
+		<div class="mt-10 md:mt-30  ">
+			<div class="text-[#D4ADFC] mt-10 text-xl font-bold" id="pitches">Pitches</div>
+			<h2 class="mt-10 text-[42px] text-white ">
+				The platform for running resiliant pitches
+			</h2>
+			<p class="mt-10 text-2xl  text-white">
+				We’ve built the leading platform for those who want to participate on new chains. <br />
+				Our engineering, protocol and security teams have built a premier protocol agnostic <br />
+				platform. Our platform removes the difficulty often associated with running node
+			</p>
+		</div>
+		<div class="mt-10 md:mt-30   ">
+			<div class="text-[#D4ADFC] mt-10 text-xl font-bold" id="contributions">About</div>
+			<h2 class="mt-10 text-[42px] text-white ">Governance and Development</h2>
+			<p class="mt-10 text-2xl  text-white">
+				We participate in governance across all active networks to ensure the interests <br />
+				of the community are fairly represented. Additionally, we are regular code contributors <br />
+				to these networks too.
+			</p>
+		</div>
+		<div class="my-10 md:my-30 ">
+			<div class="text-[#D4ADFC] mt-10 text-xl" id="contacts">Contact</div>
+			<div class="flex mt-10 justify-between">
+				<h2 class=" text-3xl text-white font-bold ">Contact us today</h2>
+				<button class="bg-[#0C134F] w-[207px] rounded=[4px] h-[54px] md:h-50 text-[#D4ADFC] text-xl  px-2 py-2">
+					Get In Touch
+				</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style lang="postcss">
+	 :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+	
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,100 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+
+	<!-- <div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div> -->
+	<header class=" bg-gradient-to-r from-[#1D267D] via-gray-[#0C134F] to-[#1D267D] text-white rounded-lg">
+		<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+		  <div class="flex lg:flex-1">
+			<a href="#" class="-m-1.5 p-1.5">
+			  <span class="sr-only">Your Company</span>
+			  <h1 class="text-white text-2xl font-bold">Your Company</h1>
+			</a>
+		  </div>
+		  <div class="flex lg:hidden">
+			<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white">
+			  <span class="sr-only">Open main menu</span>
+			  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+			  </svg>
+			</button>
+		  </div>
+		  <div class="hidden lg:flex lg:gap-x-12">
+			<a href="#sell" class="text-sm font-semibold leading-6 text-white">Sell</a>
+			<a href="#pitches" class="text-sm font-semibold leading-6 text-white">Pitches</a>
+			<a href="#contributions" class="text-sm font-semibold leading-6 text-white">About</a>
+			<a href="#contacts" class="text-sm font-semibold leading-6 text-white">Company</a>
+		  </div>
+		  <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+			<a href="/login" class="text-sm font-semibold leading-6 text-white">Log in <span aria-hidden="true">&rarr;</span></a>
+		  </div>
+		</nav>
+		<!-- Mobile menu, show/hide based on menu open state. -->
+		<div class="lg:hidden" role="dialog" aria-modal="true">
+		  <!-- Background backdrop, show/hide based on slide-over state. -->
+		  <div class="fixed inset-0 z-10"></div>
+		  <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+			<div class="flex items-center justify-between">
+			  <a href="#" class="-m-1.5 p-1.5">
+				<span class="sr-only">Your Company</span>
+				<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+			  </a>
+			  <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+				<span class="sr-only">Close menu</span>
+				<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+				  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+				</svg>
+			  </button>
+			</div>
+			<div class="mt-6 flow-root">
+			  <div class="-my-6 divide-y divide-gray-500/10">
+				<div class="space-y-2 py-6">
+				  <a href="#sell" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">Sell</a>
+				  <a href="#pitches" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">Features</a>
+				  <a href="#contributions" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">Marketplace</a>
+				  <a href="#contacts" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">Company</a>
+				</div>
+				<div class="py-6">
+				  <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50">Log in</a>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </header>
+<style>
+	
+</style>
